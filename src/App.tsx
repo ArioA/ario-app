1d73a9e980393a3ac9deb96d3765ee71d0cbf473
ArioA <ario.aliabadi@gmail.com>
2024-05-06 11:27:28 +0100

    boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Button, Separator, Text, Flex } from "@radix-ui/themes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Text size="2">
+        Ario's site
+        <Separator my="3" size="4" />
+        <Flex gap="3" align="center">
+          Themes
+          <Separator orientation="vertical" />
+          Primitives
+          <Separator orientation="vertical" />
+          Icons
+          <Separator orientation="vertical" />
+          Colors
+        </Flex>
+      </Text>
+      <Button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
